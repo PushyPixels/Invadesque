1aacf8c08a779eeed99b82fc7977251a0d1c6413
Maximilian Maroe <drakfyre@gmail.com>
2017-06-30 13:39:09 -0500

    Invadesque Part 2

---
+++ Assets/Scripts/DisplayScore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayScore : MonoBehaviour
+{
+	public Text scoreText;
+	public Text highScoreText;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		scoreText.text = "Score: " + GameManager.instance.score.ToString();
+		highScoreText.text = "High: " + GameManager.instance.highScore.ToString();
+	}
+}
---
+++ Assets/Scripts/RandomizeAnimatorStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeAnimatorStart : MonoBehaviour
+{
+	private Animator animator;
+
+	// Use this for initialization
+	void Start ()
+	{
+		animator = GetComponent<Animator> ();
+		animator.Play (animator.GetCurrentAnimatorStateInfo (0).fullPathHash, -1, Random.value);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
